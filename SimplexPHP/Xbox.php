228b8d10d6c1588b94cd553e9aec710d967d5b4a
Jordan-Hall <nukezilla@hotmail.co.uk>
2012-08-09 19:21:06 +0200

    Update SimplexPHP/Xbox.php

--- SimplexPHP/Xbox.php
+++ SimplexPHP/Xbox.php
@@ -1,9 +1,12 @@
 <?php
 /**
- * Xbox
- *
- * @author jokerhacker
- */
+  * @authors Jordan Hall <nukezilla@hotmail.co.uk> & JokerHacker
+  * @Acknowledge S.C. Chen <http://sourceforge.net/projects/simplehtmldom/>
+  * @Contributions Bennett Treptow <Upload class>
+  * @version 0.1
+  * @copyright 2012 SimplexPHP
+  */
+
 
 namespace SimplexPHP;
 use SimplexPHP\HtmlDom;
